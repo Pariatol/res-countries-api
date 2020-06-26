e208006da746e24a41fcd1e9e783cb92a64228d9
Pariatol <pierre.tromeur@gmail.com>
2020-06-26 10:37:46 +0200

    Updates

---
+++ precache-manifest.fcdc1655f27077b570ffe3b2e22a1bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d83417d6c90052cd8e385b042326d2",
+    "url": "/res-countries-api/index.html"
+  },
+  {
+    "revision": "d2c8f150a8a56b4453e3",
+    "url": "/res-countries-api/static/css/main.8fea28fb.chunk.css"
+  },
+  {
+    "revision": "a21ea8409a279cb38db0",
+    "url": "/res-countries-api/static/js/2.e970c16f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/res-countries-api/static/js/2.e970c16f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c8f150a8a56b4453e3",
+    "url": "/res-countries-api/static/js/main.46acb3af.chunk.js"
+  },
+  {
+    "revision": "43316ed13af3f504b900",
+    "url": "/res-countries-api/static/js/runtime-main.7b5244ec.js"
+  }
+]);
